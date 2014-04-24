7987727d5bdf00cd99a748befdc6ab27505d4932
Nobutaka Wakabayashi <wakabayashi@digitalsheep.co.jp>
2014-04-24 13:28:56 +0900

    bower install -S lscache
    テンプレートファイルをローカルストレージを使ってキャッシュするようにした

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,61 @@
+'use strict';
+
 jQuery(function($, undefined){
-    var init = function(){
-        console.debug('init');
-        $.get('templates/init.mst', function(template){
-            var rendered = Mustache.render(template, {name: "nqounet"});
-            console.info(rendered);
+    var DEBUG = 1,
+    lscache,
+    Mustache,
+    $runButtons,
+    generateKeyFromPath = function(path){
+        return 'fetched:' + path;
+    },
+    getTemplateForce = function(path, $cb){
+        var key = generateKeyFromPath(path);
+        $.get(path, function(template){
+            console.debug('run $.get');
+            lscache.set(key, template, 1);
+            return $cb.fire(template);
         });
+    },
+    getTemplate = function(path, $cb){
+        var key = generateKeyFromPath(path);
+        var template = lscache.get(key);
+        if (template) {
+            return $cb.fire(template);
+        }
+        else {
+            getTemplateForce(path, $cb);
+        }
+    },
+    render = function(targetId, targetName){
+        var obj = {
+            id: targetId,
+            name: targetName
+        };
+        var $cb = $.Callbacks();
+        $cb.add(function(template){
+            var rendered = Mustache.render(template, obj);
+            $('#' + targetId).html(rendered);
+        });
+        getTemplate('templates/init.mst', $cb);
+    },
+    changeName = function(e){
+        var $this = $(e.currentTarget);
+        render($this.data('id'), $this.html());
+    },
+    setVars = function(){
+        lscache = window.lscache;
+        Mustache = window.Mustache;
+        $runButtons = $('button[data-run=changeName]');
+    },
+    initHandlers = function(){
+        $runButtons.on('click', changeName);
+    },
+    init = function(){
+        setVars();
+        initHandlers();
+        if (DEBUG) {
+            lscache.flush();
+        }
     };
     init();
 });
